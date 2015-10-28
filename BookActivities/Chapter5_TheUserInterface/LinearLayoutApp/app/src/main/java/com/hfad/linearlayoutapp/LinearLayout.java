36ae4bc13f83ccfcd27d5005750c536dffe32cb4
Hamza <https://HamzaB93@bitbucket.org/HamzaB93/uwl_compsci4_lab.git>
2015-10-28 19:30:30 +0000

    Book Activity Chapter 5: Now has Linear layout and notes on how the layout works.

---
+++ BookActivities/Chapter5_TheUserInterface/LinearLayoutApp/app/src/main/java/com/hfad/linearlayoutapp/LinearLayout.java
@@ -0,0 +1,17 @@
+package com.hfad.linearlayoutapp;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LinearLayout extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+
+}
