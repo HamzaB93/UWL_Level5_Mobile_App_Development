648273f478b56c3ee0153b1b0d20aa51d7cd141b
Hamza <https://HamzaB93@bitbucket.org/HamzaB93/uwl_compsci4_lab.git>
2015-10-29 11:01:44 +0000

    Book Activity Chapter 5: Now has grid layout.

---
+++ BookActivities/Chapter5_TheUserInterface/GridLayoutApp/app/src/main/java/com/hfad/gridlayoutapp/GridLayout.java
@@ -0,0 +1,16 @@
+package com.hfad.gridlayoutapp;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class GridLayout extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_layout);
+    }
+}
