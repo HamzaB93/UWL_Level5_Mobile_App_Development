09580d45ec83fb3d53b0a8236ef4702f406510a4
Hamza <https://HamzaB93@bitbucket.org/HamzaB93/uwl_compsci4_lab.git>
2015-12-11 00:04:01 +0000

    Assingment: First commit for the assingment. This is just a test to see if an image view is able to repond to clicks. The image in the centre of the screen reponds accordingly to clicks and sends you to a test page.

---
+++ Assingment/SFVFightGuide/app/src/main/java/hamza/assignment/sfvfightguide/MainActivity.java
@@ -0,0 +1,32 @@
+package hamza.assignment.sfvfightguide;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void onClickCombo (View view)
+    {
+        Intent intent = new Intent(MainActivity.this, Test.class);
+        startActivity(intent);
+    }
+}
